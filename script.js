a011297b53f6de1486e880ac8a7ce26d25ed27ed
Milena-Arantes <milenaarantes091@gmail.com>
2024-06-10 22:20:55 -0300

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const botao = document.getElementById('btnCadastroProduto');
 
 botao.addEventListener('click', function(){
-    window.location.href = "file:///C:/Users/1050482323022/Downloads/OneDrive_2024-06-10/teste%20da%20API/frontend/Create/Cadastro_produto.html";
+    window.location.href = "../testes-Front/Create/Cadastro_produto.html";
 });
