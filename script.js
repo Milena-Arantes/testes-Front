6ac3d4da7fdeb4dc8d5c7049721d7699d53f8e45
Milena Arantes <145288690+Milena-Arantes@users.noreply.github.com>
2024-06-11 09:24:09 -0300

    create button

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-const botao = document.getElementById('btnCadastroProduto');
+const btnCadProd = document.getElementById('btnCadastroProduto');
+btnCadProd.addEventListener('click', function(){
+    window.location.href = "../testes-Front/Create/cadastro_produto.html";
+});
 
-botao.addEventListener('click', function(){
-    window.location.href = "../testes-Front/Create/Cadastro_produto.html";
+const btnCadEntrada = document.getElementsById('btnCadastroEntrada');
+btnCadEntrada.addEventListener('click', function(){
+    window.location.href = "../testes-Front/Create/cadastro_entrada.html";
+});
+
+const btnCadCat = document.getElementsById('btnCadastroCategoria');
+btnCatCat.addEventListener('click', function(){
+    window.location.href = "../testes-Front/Create/cadastro_categoria.html";
+});
+
+const btnCadForn = document.getElementsById('btnCadastroFornecedor');
+btnCadForn.addEventListener('click', function(){
+    window.location.href = "../testes-Front/Create/cadastro_fornecedor.html";
+});
+
+const btnControle = document.getElementsById('btnControleEstoque');
+btnControle.addEventListener('click', function(){
+    window.location.href = "../testes-Front/Create/cadastro_fornecedor.html";
 });
