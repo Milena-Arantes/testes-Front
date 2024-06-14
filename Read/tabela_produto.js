300bc52fcfeca0cd48fb0145f27b6e674d631205
Milena Arantes <145288690+Milena-Arantes@users.noreply.github.com>
2024-06-14 10:27:09 -0300

    Update tabela_produto.js

--- Read/tabela_produto.js
+++ Read/tabela_produto.js
@@ -1,70 +1,63 @@
-const btnVoltar = document.getElementById('btnVoltar');
-btnVoltar.addEventListener('click', function(event){
-  event.preventDefault();
-  window.history.back();
-});
-
 document.addEventListener("DOMContentLoaded", function () {
-    fetch('https://projeto-integrador-k9d3.onrender.com/api/produtos')
-        .then(response => response.json())
-        .then(data => {
-            const tableBody = document.querySelector('#listaProdutos tbody');
-            if (!tableBody) {
-                console.error('Elemento tbody não encontrado!');
-                return;
-            }
-            data.forEach((produto, index) => {
-                const row = document.createElement('tr');
-                // Simulando dados adicionais para as colunas
-                const nome = produto.nome;
-                const descricao = produto.descricao;
-                const categoria = produto.categoriaId;
-                const preco = produto.preco;
-                const quantidade = produto.estoque;
 
-                row.innerHTML = `
-                    <td>${produto.id}</td>
-                    <td>${nome}</td>
-                    <td>${descricao}</td>
-                    <td>${categoria}</td>
-                    <td>${preco}</td>
-                    <td>${quantidade}</td>
-                    <th><button class="btnEditar" data-id="${produto.id}">Editar</button></th>
-                    <th><button class="btnDeletar" data-id="${produto.id}">Deletar</button></th>
-                `;
-                tableBody.appendChild(row);
-            });
+        fetch('https://projeto-integrador-k9d3.onrender.com/api/produtos')
+            .then(response => response.json())
+            .then(data => {
+                const tableBody = document.querySelector('#listaProdutos tbody');
+                if (!tableBody) {
+                    console.error('Elemento tbody não encontrado!');
+                    return;
+                }
+                data.forEach((produto, index) => {
+                    const row = document.createElement('tr');
+                    const nome = produto.nome;
+                    const descricao = produto.descricao;
+                    const categoria = produto.categoriaId;
+                    const preco = produto.preco;
+                    const quantidade = produto.estoque;
 
-            tableBody.addEventListener('click', function(event){
-                if (event.target.classList.contains('btnEditar')) {
-                    const id = event.target.getAttribute('data-id');
-                    editarProduto(id);
-                }
-                else if(event.target.classList.contains('btnDeletar')){
-                    const id = event.target.getAttribute('data-id');
-                    const rowElement = event.target.closest('tr');
-                    deletarProduto(id, rowElement);
-                }
-            });
-        })
-        .catch(error => console.error('Erro ao carregar produtos:', error));
+                    row.innerHTML = `
+                        <td>${produto.id}</td>
+                        <td>${nome}</td>
+                        <td>${descricao}</td>
+                        <td>${categoria}</td>
+                        <td>${preco}</td>
+                        <td>${quantidade}</td>
+                        <th><button class="btnEditar" data-id="${produto.id}">Editar</button></th>
+                        <th><button class="btnDeletar" data-id="${produto.id}">Deletar</button></th>
+                    `;
+                    tableBody.appendChild(row);
+                });
+
+                tableBody.addEventListener('click', function(event) {
+                    if (event.target.classList.contains('btnEditar')) {
+                        const id = event.target.getAttribute('data-id');
+                        editarProduto(id);
+                    } else if (event.target.classList.contains('btnDeletar')) {
+                        const id = event.target.getAttribute('data-id');
+                        const rowElement = event.target.closest('tr');
+                        deletarProduto(id, rowElement);
+                    }
+                });
+            })
+            .catch(error => console.error('Erro ao carregar produtos:', error));
     });
 
     function editarProduto(id) {
         alert(`Editar produto com ID: ${id}`);
-        window.location.href = "../Update/update_produto.html?id=${id}";
-        }
+        window.location.href = `../Update/update_produto.html?id=${id}`;
+    }
 
-    function deletarProduto(id, rowElement){
+    function deletarProduto(id, rowElement) {
         const confirmDelete = confirm('Tem certeza de que deseja deletar este produto?');
-        if(confirmDelete){
-            fetch(`https://projeto-integrador-k9d3.onrender.com/api/produtos/${id}`,{
+        if (confirmDelete) {
+            fetch(`https://projeto-integrador-k9d3.onrender.com/api/produtos/${id}`, {
                 method: 'DELETE',
             })
-            .then(response=>{
-                if(response.ok){
+            .then(response => {
+                if (response.ok) {
                     rowElement.remove();
-                } else{
+                } else {
                     console.error('Erro ao deletar produto:', response.statusText);
                 }
             })
